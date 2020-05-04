9e3dc05ee7c52fd313a1adb2db1588bc64d5fd2f
Ravnoor Kambo <rkambo@uoguelph.ca>
2020-05-04 12:24:12 -0400

    Bug doesn't occur now. Now will work on selecting item

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -6,53 +6,35 @@ import { func } from 'prop-types';
 class Searchbar extends React.Component{
     constructor(props) {
         super(props);
-        this.items = ['David', 'Damien', 'Sara', 'Jane']
         this.state = {
           suggestions: [],
           text: '',
         }
       }
 
-      componentDidMount() {
-        const fetchdata = async () => {
-  
-          const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-  //          body: JSON.stringify({ name: value })
-        };
-          const response = await fetch('http://localhost:3001/searchPokemon', requestOptions);
-          const data = await response.json();
-          
-        }
-      }
-
       onTextChanged = (e) => {
         const value = e.target.value;
         let suggestions = [];
-        //this.fetchData(value);
 
         if (value.length > 0){
-          // const regex = new RegExp(`^${value}`,'i');
-          // suggestions = this.items.sort().filter(v => regex.test(v))
-          //suggestions = [{name:'Bulbasaur'}, {name:'Venusaur'}, {name: 'Charmander'}];
-          //suggestions = ['Bulbasaur', 'Venusaur', 'Charmander']
 
           const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ name: value })
         };
+        this.setState(() => ({
+          text:value.name
+        }))
           fetch('http://localhost:3001/searchPokemon', requestOptions).then(res => {
             res.json().then((response) => {
               suggestions = response;
               this.setState(() => ({
                 suggestions,
-                text : value.name
               }))
               this.renderSuggestions();
             });
-          });
+          }); 
         }
         else{
           this.setState(() => ({
@@ -70,8 +52,6 @@ class Searchbar extends React.Component{
       }
       renderSuggestions(){
         const {suggestions} = this.state
-        const {value} = this.state
-        console.log(value)
         //console.log(suggestions.results.map((item) => `<li onClick={() => this.suggestionSelected(item)}>${item.name}</li>`))
         if (suggestions.results === undefined || suggestions.results.length === 0){
           return null;
