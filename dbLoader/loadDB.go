686db2b1e52bec864221479f79a379dd45b682ed
Ravnoor Kambo <ravnoor@Ravnoors-MacBook-Pro.local>
2023-02-10 11:23:57 -0500

    Trying out go, need to implement wait groups

--- dbLoader/loadDB.go
+++ dbLoader/loadDB.go
@@ -1,23 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 )
 
-func getURLBatches(endpoint string) {
+type Pokemon struct {
+}
+
+func apiCallout(endpoint string, c chan string) (response string) {
 	resp, err := http.Get(endpoint)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
+
+	if c != nil {
+		c <- string(body)
+		return ""
+	}
+
+	return string(body)
+}
+
+func getUrlBatches() (retBatchOne []string, retBatchTwo []string) {
+	batchOne := apiCallout("https://pokeapi.co/api/v2/pokemon?limit=2", nil)
+	batchTwo := apiCallout("https://pokeapi.co/api/v2/pokemon-species?limit=2", nil)
+
+	var batchOneResponseMap map[string]any
+	json.Unmarshal([]byte(batchOne), &batchOneResponseMap)
+
+	var batchTwoResponseMap map[string]any
+	json.Unmarshal([]byte(batchTwo), &batchTwoResponseMap)
+
+	var batchOneUrls []string
+	var batchTwoUrls []string
+
+	for _, url := range batchOneResponseMap["results"].([]interface{}) {
+		name := (url.(map[string]interface{})["url"]).(string)
+		batchOneUrls = append(batchOneUrls, name)
+	}
+
+	for _, url := range batchTwoResponseMap["results"].([]interface{}) {
+		name := (url.(map[string]interface{})["url"]).(string)
+		batchTwoUrls = append(batchTwoUrls, name)
+	}
+
+	return batchOneUrls, batchTwoUrls
+}
+
+func bulkInfoRetrieve(batchOne []string, batchTwo []string) {
+	batchOneChannel := make(chan string)
+	batchTwoChannel := make(chan string)
+
+	for _, v := range batchOne {
+		go apiCalloutGo(v, batchOneChannel)
+	}
+
+	for _, v := range batchTwo {
+		go apiCalloutGo(v, batchTwoChannel)
+	}
 }
 
 func main() {
-	endpoint := "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-	getURLBatches(endpoint)
+	//endpoint := "https://pokeapi.co/api/v2/pokemon?limit=100000"
+	//endpoint2 := "https://pokeapi.co/api/v2/pokemon-species?limit=100000"
+	batchOne, batchTwo := getUrlBatches()
+	bulkInfoRetrieve(batchOne, batchTwo)
+
 }
