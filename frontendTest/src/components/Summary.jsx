7b0b606ae5d723aa757ef04e724ece5d946f8db9
Ravnoor <ravnoor595@gmail.com>
2023-03-23 08:40:12 -0400

    Fixed the bug with unknown pokemon crashing the app

--- frontendTest/src/components/Summary.jsx
+++ frontendTest/src/components/Summary.jsx
@@ -3,13 +3,25 @@ const Summary = (props) => {
     const entries = props.entries.filter(
       (entry) => entry.language.name == "en"
     );
+    if (entries.length == 0) {
+      return "Not enough information about this Pokemon has been discovered yet!";
+    }
     return entries[Math.floor(Math.random() * (entries.length - 1))]
       .flavor_text;
   };
 
+  const getCategory = () => {
+    const category = props.category;
+    if (category && category.length <= 7) {
+      return "??? Pokemon";
+    } else {
+      return <>The {category[7].genus}</>;
+    }
+  };
+
   return (
     <div>
-      {!props.category ? <h1></h1> : <h1>The {props.category[7].genus}</h1>}
+      {!props.category ? <h1></h1> : <h1>{getCategory()}</h1>}
       {!props.entries ? <h1></h1> : <h1>{getEntry()}</h1>}
     </div>
   );
